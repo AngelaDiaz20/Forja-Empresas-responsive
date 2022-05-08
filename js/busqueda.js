429dde60b6717b6dfdeeeac271fc519f5b020d34
Angela Diaz <angeldi846@gmail.com>
2022-05-08 01:44:06 -0500

    second case of evaluation for patient search is added

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,4 +1,4 @@
-let button = document.getElementById('searchUser');
+let button = document.getElementById('searchUser'); 
 
 button.onclick = function search(e){
     e.preventDefault();
@@ -12,9 +12,9 @@ button.onclick = function search(e){
         searchResult.innerHTML = `
         <h3> Pepito Pérez </h3>
         <fieldset>
-            <legend> <strong> Primera vacuna </Strong> </legend>
-            <p>laboratorio: Pfizer</p
-            <p>Fecha de aplicación: 20/01/2022</p>
+            <legend> <strong> Primera dosis </Strong> </legend>
+            <p> <strong>laboratorio:</strong> Pfizer</p
+            <p> <strong>Fecha de aplicación:</strong> 20/01/2022</p>
         </fieldset>
         <h4>¿Deseas registrar datos de vacunación?</h4>
         <div class="buttons-modal">
@@ -32,12 +32,46 @@ button.onclick = function search(e){
             window.location.href="#modal"
         },500);
     }
+
+    else if(numberDocument == 1001235478){
+        console.log("Correcto")
+        searchResult.innerHTML = `
+        <h3> Panaberto Pecopón </h3>
+        <fieldset>
+            <legend> <strong> Primera dosis </Strong> </legend>
+            <p> <strong>laboratorio:</strong> Pfizer</p
+            <p> <strong>Fecha de aplicación:</strong> 25/10/2021</p>
+        </fieldset>
+
+        <fieldset>
+            <legend> <strong> Segunda dosis </Strong> </legend>
+            <p> <strong>laboratorio:</strong> Pfizer</p
+            <p> <strong>Fecha de aplicación:</strong> 20/02/2022</p>
+        </fieldset>
+
+        <h4>¿Deseas registrar datos de vacunación?</h4>
+        <div class="buttons-modal">
+            <a href="registro.html">
+                <button class="bton-login">¡Si!, gracias
+                </button>
+            </a>
+            <a href="#dashboard">
+                <button class="bton-register">Cancelar</button>
+            </a>
+        </div>
+        `;
+        window.setTimeout(()=>{
+            window.location.href="#modal"
+        },500);
+    }
+
     else{
         window.setTimeout(()=>{
             window.location.href="#modal"
         },500);
         searchResult.innerHTML = `
-        <h3>Usuario 2no registrado</h3>
+        <h3>Usuario no registrado</h3>
+        <h4>¿Deseas registrar un nuevo paciente?</h4>
         <div class="buttons-modal">
             <a href="registro.html">
                 <button class="bton-login">¡Si!, gracias
